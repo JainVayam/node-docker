9631755c8007c3a2e8708ae4cde0c5442c8596e3
Vayam <vayam.jain@gmail.com>
2021-06-30 00:09:46 +0530

    Testing changes

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express'
 
 const app = express()
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
-app.listen(port)
+app.listen(port, () => console.log("Hello Worlasd"))
 
 app.get('/ping', (req, res) => {
-  res.send("Tested Ok")
+  console.log("PARFAIT")
+  res.send("Tes123123teasdasdd dqwd Casdwqwehanged")
 })

331869829a7a90f87c24a1ab247f5db604a7aa18
Vayam <vayam.jain@gmail.com>
2021-07-03 19:45:25 +0530

    Added express session

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import session from 'express-session'
+import redis from 'redis'
+import Redis from 'connect-redis'
 
 import config from './config/config.js'
 import postRouter from './routes/postRoute.js'
 import authRouter from './routes/authRouter.js'
 
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = config
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}/${MONGO_PORT}?authSource=admin`
+const { SESSION_SECRET, REDIS_URL, REDIS_PORT, MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, MONGO_DB } = config
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
+const RedisStore = Redis(session)
 
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
@@ -16,17 +20,34 @@ mongoose.connect(mongoURL, {
   .then(() => console.log("Successfully Connected"))
   .catch(e => console.log("Error is here", e))
 
+const redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT
+})
+
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(session({
+  store: new RedisStore({ client: redisClient }),
+  secret: SESSION_SECRET,
+  cookie: {
+    secure: false,
+    resave: false,
+    saveUninitialized: false,
+    httpOnly: true,
+    maxAge: 30000
+  }
+}))
+
+
 app.use('/api/v1/posts', postRouter)
 app.use('/api/v1/user', authRouter)
 app.get('/ping', (req, res) => {
-  console.log("PARFAIT")
-  res.send("Tes123123teasdasdd dqwd Casdwqwehanged")
+  res.send("Tested Ok")
 })
 
-app.listen(port, () => console.log("Hello Worlasd"))
+app.listen(port, () => console.log("Hello World"))
 
